567679b0f91a940aec646a886524bcca34286f25
AncutaBiro <ancuta.biro@yahoo.com>
2020-11-09 00:12:13 +0200

    solved

---
+++ Functions3/Functions3/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Se citește de la tastatură o dată calendaristică în următorul format:
+
+ziua pe prima linie
+luna în format numeric pe linia 2 (de la 1 la 12)
+anul pe următoarea linie
+Pe următoarea linie e o opțiune ce indică formatul în care vrem să tipărim data citită. Există 3 variante:
+
+opțiunea 1: se afișează ziua + spațiu + luna în format text (de exemplu: 1 iunie)
+opțiunea 2: se afișează luna în format text + spațiu + anul (de exemplu: iunie 2017)
+opțiunea 3: se afișează ziua + spațiu + luna în format text + spațiu + anul (de exemplu: 1 iunie 2017)
+Exemplu:
+
+Pentru datele de intrare:
+
+1
+6
+2017
+3
+La consolă se va afișa:
+
+1 iunie 2017
+ 
+ */
+
+
+using System;
+
+namespace Concat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = { "ianuarie", "februarie", "martie", "aprilie", "mai", "iunie",
+                "iulie", "august", "septembrie", "octombrie", "noiembrie", "decembrie"};
+
+            string day = Console.ReadLine();
+            string month = months[Convert.ToInt32(Console.ReadLine()) - 1];
+            string year = Console.ReadLine();
+
+            PrintDate(day, month, year, Console.ReadLine());
+            Console.Read(); ;
+        }
+
+        static void PrintDate(string day, string month, string year, string option)
+        {
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine(Concat(day, month));
+                    break;
+                case "2":
+                    Console.WriteLine(Concat(month, year));
+                    break;
+                case "3":
+                    Console.WriteLine(Concat(day, month, year));
+                    break;
+            }
+
+        }
+
+        static string Concat(params string[] words)
+        {
+            string result = "";
+
+            foreach (string s in words)
+            {
+                result += s + " ";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Functions4/Functions4/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+La un concurs de cunoștințe generale se pornește cu o sumă de bani dată inițial și apoi concurenții trebuie să răspundă corect la 9 întrebări. Fiecare răspuns corect dublează suma câștigată, exceptând răspunsurile la întrebările 3, 6 și 9 care triplează suma.
+
+Scrie o aplicație ce primește suma inițială și numărul de răspunsuri corecte date de un concurent și afișează suma totală câștigată de acel concurent.
+
+Exemplu:
+
+Pentru datele de intrare:
+
+3
+9
+La consolă se va afișa:
+
+5184
+ */
+
+using System;
+
+namespace KnowledgeContest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long initialAmount = Convert.ToInt64(Console.ReadLine());
+            int questionsAnswered = Convert.ToInt32(Console.ReadLine());
+
+            long amountEarned = initialAmount;
+            for (int i = 1; i <= questionsAnswered; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    Multiply(ref amountEarned);
+                }
+                else
+                {
+                    Multiply(ref amountEarned, 3);
+                }
+
+            }
+
+            Console.WriteLine(amountEarned);
+            Console.Read();
+        }
+
+        static void Multiply(ref long baseValue, int multiplier = 2)
+        {
+            baseValue = baseValue * multiplier;
+        }
+    }
+}
