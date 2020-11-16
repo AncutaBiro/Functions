9214613fe845ce7c3d240485673344290e2baff5
AncutaBiro <ancuta.biro@yahoo.com>
2020-11-16 21:51:13 +0200

    gcd and lcm

---
+++ Functions5.1/Functions5.1/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Se dau două numere întregi pozitive diferite. Să se afișeze pe căte o linie cel mai mare divizor comun (cmmdc) și cel mai mic multiplu comun (cmmmc) a celor două numere.
+Exemplu:
+Pentru datele de intrare:
+6
+4
+La consolă se va afișa:
+
+2
+12
+ 
+ */
+
+using System;
+
+namespace Functions5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Cmmdc(a, b));
+            Console.WriteLine(Cmmmc(a, b));
+        }
+
+        static int Minimum(int a, int b)
+        {
+            {
+                int min = a < b ? a : b;
+
+                return min;
+            }
+        }
+
+        /*initialize cmmdc with 1 in case of 2 prime number that will both only by 1. 
+         * The smallest cmmdc is 2, so starting from 2 until the min (a, b) check each number that will divide both numbers without a reminder => finding the greatest common factor */
+        static int Cmmdc(int a, int b)
+        {
+            int cmmdc = 1;
+
+            int min = Minimum(a, b);
+
+            for (int i = 2; i <= min; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    cmmdc = i;
+                }
+            }
+            return cmmdc;
+
+        }
+
+        /* extract cmmmc after formula  a * b = LCM(a, b) * GCD(a, b) */
+
+        static int Cmmmc(int a, int b)
+        {
+            int p = a * b;
+
+            return p / Cmmdc(a, b);
+
+        }
+
+    }
+}
+
--- Functions5/Functions5/Program.cs
+++ Functions5/Functions5/Program.cs
@@ -8,24 +8,38 @@ namespace Functions5
         static void Main(string[] args)
         {
 
-            int n = 24;
-            int m = 40;
+            int n = 84;
+            int m = 196;
+
+            int[] decompositionA = DecompositionInPrimeNumbers(n);
+            int[] decompositionB = DecompositionInPrimeNumbers(m);
+
+            /*List<int> a = new List<int> {2,2,3,7,5};
+            List<int> b = new List<int> {2,7,2,5};
+
+            foreach (int k in Find (a, b))
+                Console.Write(k + " ");*/
+
+            Console.WriteLine();
 
 
-
-            /*            foreach(int j in PrimeNumbers(n))
-                        Console.Write(j + " ");
-
-                        Console.WriteLine("\n");*/
-
-
-
-
-            foreach (int j in DecompositionInPrimeNumbers(n))
+            foreach (int j in decompositionA)
                 Console.Write(j + " ");
 
-            foreach (int j in DecompositionInPrimeNumbers(m))
+            Console.WriteLine();
+
+            OccurencesOfElementInArray(decompositionA);
+
+
+            foreach (int j in decompositionB)
                 Console.Write(j + " ");
+
+            Console.WriteLine();
+
+            foreach (int k in FindCommonElements(decompositionA, decompositionB))
+            Console.Write(k);
+
+
 
         }
 
@@ -89,23 +103,105 @@ namespace Functions5
         }
 
 
-        static int DuplicateInArray(int[] array)
+        static int OccurencesOfElementInArray(int[] array)
         {
-
-            int count = 1;
+            int count = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
-                for (int j = i; j < array.Length - 1; j++)
+                count = 0;
+
+                for (int j = 0; j < array.Length; j++)
                 {
-                    if (array[i] == array[j + 1])
+                    if (array[i] == array[j])
                         count = count + 1;
                 }
-                /*                Console.WriteLine("\t\n " + array[i] + "occurse" + count);
-                                Console.ReadKey();*/
+
+                Console.WriteLine(array[i] + "occurs" + count + "times");
             }
 
             return count;
+        }
+
+/*
+        int[] array = { 10, 5, 10, 2, 2, 3, 4, 5, 5, 6, 7, 8, 9, 11, 12, 12 };
+for (int i = 0; i<array.Length; i++)
+{
+    int count = 0;
+    for (int j = 0; j<array.Length ; j++)
+    {
+       if(array[i] == array[j])
+          count = count + 1;
+    }
+    Console.WriteLine("\t\n " + array[i] + " occurs " + count);
+    Console.ReadKey();*/
+
+
+
+
+
+static int[] FindCommonElements(int[] a1, int[] a2)
+        {
+            int count = 0;
+
+            for (int i = 0; i < a1.Length; i++)
+            {
+                for (int j = 0; j < a2.Length; j++)
+                {
+                    if (a1[i] == a2[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            int[] result = new int[count];
+
+            count = 0;
+            for (int i = 0; i < a1.Length; i++)
+            {
+                for (int j = 0; j < a2.Length; j++)
+                {
+                    if (a1[i] == a2[j])
+                    {
+                        result[count++] = a1[i];
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+
+        static List<int> Find(List <int> a1, List <int> a2) 
+       
+        {
+
+            List<int> c = new List<int>();
+
+            foreach (int a in a1)
+            {
+                if (a2.Contains(a))
+
+                    c.Add(a);
+            }
+
+            for (int i = 0; i < c.Count; i++)
+            {
+                for (int j = i; j < c.Count-1; j++)
+                {
+                    if (c[i] == c[j])
+                    {
+                        c.Remove(c[i]);
+                
+                    }
+                }
+            }
+
+            return c;
         }
     }
 }
